47628c343d7fde64a36dd23e77f4988a8bc8f8e8
zhixing1996 <jingmaoqiang1996@163.com>
2018-05-10 09:05:15 +0800

    add a way to slect sample

--- scripts/sel_events.py
+++ scripts/sel_events.py
@@ -7,50 +7,66 @@ __author__='JING Maoqiang <jingmq@ihep.ac.cn>'
 __copyright__='Copyright (c) JING Maoqiang'
 __created__='[2018-04-22]'
 
-import sys
 import os
 import math
 import ROOT
 from array import array
 
-args=sys.argv[1:]
+def sel(infile,outfile):
+	fin=ROOT.TFile(infile)
+	t=fin.Get('MCPart')
+	Mass_Recoil=array('d',[0])
+	fout=ROOT.TFile(outfile,'RECREATE')
+	t_out=ROOT.TTree('MCPart','MCPart')
+	t_out.Branch('Mass_Recoil',Mass_Recoil,'Mass_Recoil/D')
+	Count=0
+	N=[0.,0.,0.,0.,0.,0.,0.,0.]
+	for ientry in xrange(t.GetEntries()):
+		t.GetEntry(ientry)
+		if ((t.m_n_leptonp+t.m_n_leptonm)<2|(t.m_n_leptonp+t.m_n_leptonm))>3:
+			continue
+		N[0]+=1
+		if (t.m_n_charged>3):
+			continue
+		N[1]+=1
+		if (t.m_n_gamma>1):
+			continue
+		N[2]+=1
+		if (t.m_pt_dilepton<=10 or t.m_pt_dilepton>=70):
+			continue
+		N[3]+=1
+		if (abs(t.m_p_dilepton[2])>=60):
+			continue
+		N[4]+=1
+		# if (abs(math.cos(t.m_angle_dilepton))>=0.8):
+		if (abs(t.m_cos_Z)>=0.8):
+			continue
+		N[5]+=1
+		if (t.m_m_visble<=70 or t.m_m_visble>=100):
+			continue
+		N[6]+=1
+		if (t.m_energy_visble<=90 or t.m_energy_visble>=120):
+			continue
+		N[7]+=1
+		Mass_Recoil[0]=t.m_m_recoil
+		t_out.Fill()
+	print '######Cut flow######'
+	print 'InputFile: %s'%(infile)
+	print 'Total Events: %d'%(t.GetEntries())
+	for j in xrange(8):
+		print 'Cut No.: %d'%(j+1)
+		print 'Events: %d'%(N[j])
+		print 'Efficiency: %f'%(N[j]/t.GetEntries())
+	
+	fout.Write()
+	fout.Close()
 
-if (len(args)<2):
-	print 'input error'
-	exit(0)
-
-infile=args[0]
-outfile=args[1]
-
-fin=ROOT.TFile(infile)
-t=fin.Get('MCPart')
-
-Mass_Recoil=array('d',[0])
-
-fout=ROOT.TFile(outfile,'RECREATE')
-t_out=ROOT.TTree('MCPart','MCPart')
-t_out.Branch('Mass_Recoil',Mass_Recoil,'Mass_Recoil/D')
-
-for ientry in xrange(t.GetEntries()):
-	t.GetEntry(ientry)
-	if ((t.m_n_leptonp+t.m_n_leptonm)<2|(t.m_n_leptonp+t.m_n_leptonm))>3:
-		continue
-	if (t.m_n_charged>3):
-		continue
-	if (t.m_n_gamma>1):
-		continue
-	if (t.m_pt_dilepton<=10 or t.m_pt_dilepton>=70):
-		continue
-	if (abs(t.m_p_dilepton[2])>=60):
-		continue
-	if (abs(math.cos(t.m_angle_dilepton))>=0.8):
-		continue
-	if (t.m_m_visble<=70 or t.m_m_visble>=100):
-		continue
-	if (t.m_energy_visble<=90 or t.m_energy_visble>=120):
-		continue
-	Mass_Recoil[0]=t.m_m_recoil
-	t_out.Fill()
-
-fout.Write()
-fout.Close()
+if __name__ == '__main__':
+	sel('../rawdata/signal_e1e1h_1.root','../presel/signal_e1e1h_1.root')
+	sel('../data/bhabha.root','../presel/bhabha.root')
+	sel('../data/e3e3.root','../presel/e3e3.root')
+	sel('../data/sze_l0mu.root','../presel/sze_l0mu.root')
+	sel('../data/sze_l0nunu.root','../presel/sze_l0nunu.root')
+	sel('../data/sze_l0tau.root','../presel/sze_l0tau.root')
+	sel('../data/szeorsw_l0l.root','../presel/szeorsw_l0l.root')
+	sel('../data/zz_l0taumu.root','../presel/zz_l0taumu.root')
--- submit.sh
+++ submit.sh
@@ -10,8 +10,9 @@ usage() {
 	printf "\n\t%-5s\n" "./submit.sh [OPTION]" 
 	printf "\nOPTIONS\n" 
 	printf "\n\t%-5s  %-40s\n"  "0.1.1"    "Run on signal and background samples" 
-	printf "\n\t%-5s  %-40s\n"  "0.1.2"    "Select candidates from signal and background samples" 
-	printf "\n\t%-5s  %-40s\n"  "0.1.3"    "Draw plots of signal and background" 
+	printf "\n\t%-5s  %-40s\n"  "0.1.2"    "Synthetize seperated ROOT files" 
+	printf "\n\t%-5s  %-40s\n"  "0.1.3"    "Select candidates from signal and background samples" 
+	printf "\n\t%-5s  %-40s\n"  "0.1.4"    "Draw plots of signal and background" 
 	printf "\nDATE\n"
 	printf "\n\t%-5s\n" "AUGUST 2016"     
 }
@@ -26,23 +27,48 @@ option=$1
 
 case $option in 
 	0.1.1) echo "Running on signal and background samples..."
-		unset MARLIN_DLL
-		export MARLIN_DLL=./lib/libhig2inv.so
-		rm rawdata -rf
-		mkdir rawdata
-		Marlin steer/signal.steer 
-		# Marlin steer/bkg.steer 
+		if [ ! -d "rawdata" ]; then
+			mkdir rawdata
+		fi
+		rm job/job.out -rf
+		mkdir  job/job.out
+		rm job/job.err -rf
+		mkdir job/job.err
+		cd job
+		./signal_e1e1h.sh
+		./bhabha.sh
+		./e3e3.sh
+		./sze_l0mu.sh
+		./sze_l0nunu.sh
+		./sze_l0tau.sh
+		./szeorsw_l0l.sh
+		./zz_l0taumu.sh
 	;;
 
-	0.1.2) echo "Selecting events from signal and background samples..."
+	0.1.2) echo "Synthetizing seperated ROOT files..."
+		if [ ! -d "data" ]; then
+			mkdir data
+		fi
+		cd data
+		hadd signal_e1e1h.root ../rawdata/signal_e1e1h*
+		hadd bhabha.root ../rawdata/bhabha*
+		hadd e3e3.root ../rawdata/e3e3*
+		hadd sze_l0mu.root ../rawdata/sze_l0mu*
+		hadd sze_l0nunu.root ../rawdata/sze_l0nunu*
+		hadd sze_l0tau.root ../rawdata/sze_l0tau*
+		hadd szeorsw_l0l.root ../rawdata/szeorsw_l0l*
+		hadd zz_l0taumu.root ../rawdata/zz_l0taumu*
+	;;
+
+	0.1.3) echo "Selecting events from signal and background samples..."
 		if [ ! -d "presel" ]; then
 			mkdir presel
 		fi
-		python scripts/sel_events.py rawdata/signal.root presel/signal.root
-		python scripts/sel_events.py rawdata/signal.root presel/bkg.root
+		cd scripts
+		python sel_events.py
 	;;
 
-	0.1.3) echo "Drawing plots of signal and background..."
+	0.1.4) echo "Drawing plots of signal and background..."
 		if [ ! -d "figs" ]; then
 			mkdir figs
 		fi
