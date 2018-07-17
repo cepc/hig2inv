8a7d85b6bd0f4339e396b47df1c4bc32b5962b62
zhixing1996 <jingmaoqiang1996@163.com>
2018-07-17 17:11:46 +0800

    polishing submit.sh

--- submit.sh
+++ submit.sh
@@ -39,55 +39,15 @@ case $option in
 		rm job/job.err -rf
 		mkdir job/job.err
 		cd job
-
-		./signal_e2e2h.sh
-		./bkg_e2e2h_sw_l.sh
-		./bkg_e2e2h_sw_sl.sh
-		./bkg_e2e2h_qq.sh
-		./bkg_e2e2h_szeorsw_l.sh
-		./bkg_e2e2h_sze_sl.sh
-		./bkg_e2e2h_sznu_l.sh
-		./bkg_e2e2h_sznu_sl.sh
-		./bkg_e2e2h_ww_h.sh
-		./bkg_e2e2h_sze_l.sh
-		./bkg_e2e2h_ww_l.sh
-		./bkg_e2e2h_ww_sl.sh
-		./bkg_e2e2h_zz_h.sh
-		./bkg_e2e2h_zz_l.sh
-		./bkg_e2e2h_zzorww_h.sh
-		./bkg_e2e2h_zzorww_l.sh
-		./bkg_e2e2h_zz_sl0mu_down.sh
-		./bkg_e2e2h_zz_sl0mu_up.sh
-		./bkg_e2e2h_zz_sl0nu_down.sh
-		./bkg_e2e2h_zz_sl0nu_up.sh
+		hep_sub -g physics run_sample_job -e job.err/ -o job.out/
 	;;
 
 	0.1.2) echo "Synthetizing seperated ROOT files..."
 		if [ ! -d "presel" ]; then
 			mkdir presel
 		fi
-		cd presel
-		hadd signal_e2e2h.root ../splitted/signal_e2e2h*
-		hadd bkg_e2e2h_sw_l.root ../splitted/bkg_e2e2h_sw_l*
-		hadd bkg_e2e2h_sw_sl.root ../splitted/bkg_e2e2h_sw_sl*
-		hadd bkg_e2e2h_qq.root ../splitted/bkg_e2e2h_qq*
-		hadd bkg_e2e2h_szeorsw_l.root ../splitted/bkg_e2e2h_szeorsw_l*
-		hadd bkg_e2e2h_sze_sl.root ../splitted/bkg_e2e2h_sze_sl*
-		hadd bkg_e2e2h_sznu_l.root ../splitted/bkg_e2e2h_sznu_l*
-		hadd bkg_e2e2h_sznu_sl.root ../splitted/bkg_e2e2h_sznu_sl*
-		hadd bkg_e2e2h_ww_h.root ../splitted/bkg_e2e2h_ww_h*
-		hadd bkg_e2e2h_sze_l.root ../splitted/bkg_e2e2h_sze_l*
-		hadd bkg_e2e2h_ww_l.root ../splitted/bkg_e2e2h_ww_l*
-		hadd bkg_e2e2h_ww_sl.root ../splitted/bkg_e2e2h_ww_sl* 
-		hadd bkg_e2e2h_zz_h.root ../splitted/bkg_e2e2h_zz_h*
-		hadd bkg_e2e2h_zz_l.root ../splitted/bkg_e2e2h_zz_l*
-		hadd bkg_e2e2h_zzorww_h.root ../splitted/bkg_e2e2h_zzorww_h*
-		hadd bkg_e2e2h_zzorww_l.root ../splitted/bkg_e2e2h_zzorww_l*
-		hadd bkg_e2e2h_zz_sl0mu_down.root ../splitted/bkg_e2e2h_zz_sl0mu_down*
-		hadd bkg_e2e2h_zz_sl0mu_up.root ../splitted/bkg_e2e2h_zz_sl0mu_up*
-		hadd bkg_e2e2h_zz_sl0nu_down.root ../splitted/bkg_e2e2h_zz_sl0nu_down*
-		hadd bkg_e2e2h_zz_sl0nu_up.root ../splitted/bkg_e2e2h_zz_sl0nu_up*
-		hadd bkg_e2e2h.root bkg_e2e2h_*root
+		cd job
+		./hadd.sh
 	;;
 
 	0.1.3) echo "Drawing distributions of cut variables and calculate ratios of bachground over signal..."
@@ -98,35 +58,18 @@ case $option in
 			mkdir logfiles
 		fi
 		cd job
-		hep_sub -g physics cut_variable_job -e job.err -o job.out
+		hep_sub -g physics cut_variable_job -e job.err/ -o job.out/
 	;;
 
 	0.1.4) echo "Calculating cut flows of siganl and background samples..."
 		if [ ! -d "sel" ]; then
 			mkdir sel
 		fi
-		cd scripts
-		python sel_events_e2e2h.py ../presel/signal_e2e2h.root ../sel/signal_e2e2h.root
-		python sel_events_e2e2h.py ../presel/bkg_e2e2h_qq.root ../sel/bkg_e2e2h_qq.root
-		python sel_events_e2e2h.py ../presel/bkg_e2e2h_sw_l.root ../sel/bkg_e2e2h_sw_l.root
-		python sel_events_e2e2h.py ../presel/bkg_e2e2h_sw_sl.root ../sel/bkg_e2e2h_sw_sl.root
-		python sel_events_e2e2h.py ../presel/bkg_e2e2h_szeorsw_l.root ../sel/bkg_e2e2h_szeorsw_l.root
-		python sel_events_e2e2h.py ../presel/bkg_e2e2h_sze_sl.root ../sel/bkg_e2e2h_sze_sl.root
-		python sel_events_e2e2h.py ../presel/bkg_e2e2h_sznu_l.root ../sel/bkg_e2e2h_sznu_l.root
-		python sel_events_e2e2h.py ../presel/bkg_e2e2h_sznu_sl.root ../sel/bkg_e2e2h_sznu_sl.root
-		python sel_events_e2e2h.py ../presel/bkg_e2e2h_ww_h.root ../sel/bkg_e2e2h_ww_h.root
-		python sel_events_e2e2h.py ../pesel/bkg_e2e2h_zzorww_h.root ../sel/bkg_e2e2h_zzorww_h.root
-		python sel_events_e2e2h.py ../pesel/bkg_e2e2h_zzorww_l.root ../sel/bkg_e2e2h_zzorww_l.root
-		python sel_events_e2e2h.py ../presel/bkg_e2e2h_sze_l.root ../sel/bkg_e2e2h_sze_l.root
-		python sel_events_e2e2h.py ../presel/bkg_e2e2h_ww_l.root ../sel/bkg_e2e2h_ww_l.root
-		python sel_events_e2e2h.py ../presel/bkg_e2e2h_ww_sl.root ../sel/bkg_e2e2h_ww_sl.root
-		python sel_events_e2e2h.py ../presel/bkg_e2e2h_zz_h.root ../sel/bkg_e2e2h_zz_h.root
-		python sel_events_e2e2h.py ../presel/bkg_e2e2h_zz_l.root ../sel/bkg_e2e2h_zz_l.root
-		python sel_events_e2e2h.py ../presel/bkg_e2e2h_zz_sl0mu_down.root ../sel/bkg_e2e2h_zz_sl0mu_down.root
-		python sel_events_e2e2h.py ../presel/bkg_e2e2h_zz_sl0mu_up.root ../sel/bkg_e2e2h_zz_sl0mu_up.root
-		python sel_events_e2e2h.py ../presel/bkg_e2e2h_zz_sl0nu_down.root ../sel/bkg_e2e2h_zz_sl0nu_down.root
-		python sel_events_e2e2h.py ../presel/bkg_e2e2h_zz_sl0nu_up.root ../sel/bkg_e2e2h_zz_sl0nu_up.root
-		python sel_events_e2e2h.py ../presel/bkg_e2e2h.root ../sel/bkg_e2e2h.root
+		if [ ! -d "logfiles" ]; then
+			mkdir logfiles
+		fi
+		cd job
+		hep_sub -g physics cut_flow_job -e job.err/ -o job.out/
 	;;
 
 	0.1.5) echo "Drawing plots of signal and background..."
